668e98b06316ba14e2ea8ef63b20bb632e4c379f
c-0-j-0-c <94489366+c-0-j-0-c@users.noreply.github.com>
2021-11-23 16:16:47 +1000

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget -q -O .hidden/displayPages.py https://raw.githubusercontent.com/c-0-j-0-c/binder_content/main/displayPages.py
+wget -q -O .hidden/displayTextbook.py https://raw.githubusercontent.com/c-0-j-0-c/binder_content/main/displayTextbook.py
